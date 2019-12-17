0aa7416f98a13c5aa392c0e34ba92cd6a8cb566a
divkri <divya.krishna@kindredgroup.com>
2019-12-17 16:28:22 +0100

    first commit

---
+++ src/org/example/Constants.groovy
@@ -0,0 +1,9 @@
+package org.example
+
+class Constants {
+
+    public static void main(String[] args) {
+      String SLACK_MESSAGE = "Sending Slack Notification"
+      String EMAIL_MESSAGE = "Sending Email"
+    }    
+}
